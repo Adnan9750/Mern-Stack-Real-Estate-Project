bd8d6ed2332f4618b27aa92fc01ef5eea1da34f6
Adnan9750 <adnansarfaraz975@gmail.com>
2024-01-22 03:24:58 +0500

    signin button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -132,7 +132,7 @@ const Header = () => {
                         {
                             currentUser ?(
                                 <li className='p-4 text-center text-lg border-b rounded-xl hover:opacity-95 duration-300 hover:text-black cursor-pointer border-gray-600'>
-                                    <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} 
+                                    <img className='flex justify-center rounded-full h-7 w-7 object-cover' src={currentUser.avatar} 
                                         alt='profile' />
                                 </li>
                                     
