bd11e136a39412a07c7980aae5c26eacc25a6ba5
Adnan9750 <adnansarfaraz975@gmail.com>
2024-01-21 20:46:48 +0500

    Cors origin change

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(express.json())
 
 // app.use(cors());
 app.use(cors({
-    origin: 'https://mern-real-estate-fpt2.onrender.com',
+    origin: '*',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     optionsSuccessStatus: 204, // Allow cookies to be sent from the frontend
